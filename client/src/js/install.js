f3ca2c69db92723747e14ca60ec4494de9b54b49
MegMathis <mcdonough.megan25@gmail.com>
2023-05-03 19:19:55 -0400

    install js wip

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -14,7 +14,17 @@ window.addEventListener("beforeinstallprompt", (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {});
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  // see the prompt
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  // hide the button
+  butInstall.classList.toggle("hidden", true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {});
